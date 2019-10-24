117043601fee407f9d581492e0afc071e1208a03
thirupparan <thirupparan1994@gmail.com>
2019-10-25 00:56:18 +0530

    services added

---
+++ servers/spring-eureka-zuul/src/test/java/com/eureka/zuul/SpringEurekaZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eureka.zuul;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEurekaZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
